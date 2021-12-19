5c058fca54d6b683d6ee7fb83cb1d4b2198f61b9
rockan007 <rockan007@gmail.com>
2021-12-19 08:46:33 +0000

    computed fields and  onchanges

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 class EstateProperty(models.Model):
     _name="estate.property"
@@ -27,3 +27,25 @@ class EstateProperty(models.Model):
     partner_id = fields.Many2one('res.partner',string="Buyer",index= True,copy = False)
     tag_ids = fields.Many2many('estate.property.tag',string="tags")
     offer_ids = fields.One2many('estate.property.offer',"property_id",string="Offers")
+
+    total_area = fields.Integer(compute="_compute_total_area")
+    best_price = fields.Float(compute="_compute_best_price")
+
+    @api.depends('living_area','garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area= record.living_area+record.garden_area
+    
+    @api.depends('offer_ids')
+    def _compute_best_price(self):
+        for record in self:
+            record.best_price= max(record.offer_ids.mapped('price'))
+
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation='north'
+        else:
+            self.garden_area = 0
+            self.garden_orientation = ""
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import api, fields, models
+import datetime
 
 class EstatePropertyOffer(models.Model):
     _name="estate.property.offer"
@@ -14,4 +15,16 @@ class EstatePropertyOffer(models.Model):
     partner_id = fields.Many2one('res.partner', string="Partner",required=True)
     property_id = fields.Many2one('estate.property',required=True)
 
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute="_compute_date_deadline",inverse="_inverse_date_deadline")
+
+
+    @api.depends('create_date','validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            record.date_deadline = (record.create_date if record.create_date else datetime.date.today()) +datetime.timedelta(days = record.validity)
+    
+    def _inverse_date_deadline(self):
+        for record in self:
+            record.validity = (record.date_deadline-record.create_date.date()).days
 
