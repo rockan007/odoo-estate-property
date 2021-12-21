1467f40c680020c0bb74687f7e5f9f9df42eb4e6
rockan007 <rockan007@gmail.com>
2021-12-21 12:25:17 +0000

    inline views

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -5,6 +5,7 @@ class EstatePropertyType(models.Model):
     _description="type of estate property"
     
     name = fields.Char(required=True)
+    property_ids = fields.One2many('estate.property','property_type_id',string="Property Ids")
 
     _sql_constraints = [
         ('name_uniq', 'unique (name)', 'The name of the estate property type must be unique!'),
